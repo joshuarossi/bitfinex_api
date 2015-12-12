00b2381551e29dba5843c2b1301ecdf2ad40a0a2
Tomas Corellano <corellanot@gmail.com>
2015-12-12 07:31:45 -0300

    updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nevtep:bitfinex-api',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'A simple wrapper that allows one to interact with bitfinex, without dependency of iron router',
   // URL to the Git repository containing the source code for this package.
